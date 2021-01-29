025aca745446a6d80d848ea012fde28f66e30b76
Дмитрий А. Воробьев <dmitryvorobev@yoomoney.ru>
2021-01-29 19:17:55 +0300

    and fix workdays

--- app/assistant.py
+++ app/assistant.py
@@ -500,7 +500,7 @@ if __name__ == "__main__":
     scheduler.add_job(duty_reminder_daily_morning, 'cron', day_of_week='*',  hour=9, minute=45)
     scheduler.add_job(duty_reminder_daily_evening, 'cron', day_of_week='mon,tue,wed,thu',  hour=18, minute=30)
     scheduler.add_job(duty_reminder_weekend, 'cron', day_of_week='fri', hour=14, minute=1)
-    scheduler.add_job(duty_reminder_tststnd_daily, 'cron', day_of_week='*', hour=10, minute=00)
+    scheduler.add_job(duty_reminder_tststnd_daily, 'cron', day_of_week='mon-fri', hour=10, minute=00)
 
     # Who is next?
     scheduler.add_job(lambda: call_who_is_next(jira_connect), 'interval', minutes=1, max_instances=1)
